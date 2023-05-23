e3cdba21fb9b8db0272abe8da6eb752dec21ea43
Benjamin Funke <58399929+CrystalBen@users.noreply.github.com>
2023-05-23 17:58:50 +0200

    added DecompOBC

---
+++ DecompOBC/DecompOBC/DecompOBC.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Text;
+Console.WriteLine("DecompOBC ver. 1.0");
+Console.WriteLine("Made by CrystalBen");
+Console.WriteLine("How to use?");
+Console.WriteLine("type DecompOBC.exe then enter, type your OBC Script Path including your .obc File (without quotes), then it shows you the Text of the Script.");
+Console.WriteLine("\n");
+Console.WriteLine("Write here the Path to the OBC File");
+
+string OBC_OUTPUT = Console.ReadLine();
+
+
+{
+    Console.WriteLine("\n");
+            // Output the Text of the OBC Script.
+            Console.WriteLine(File.ReadAllText(@OBC_OUTPUT));
+
+
+
+}
