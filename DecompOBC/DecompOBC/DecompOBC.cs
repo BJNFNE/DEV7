1104f4f46030d0f18c6dbb613c4d21670d1ec1fc
Benjamin Funke <58399929+CrystalBen@users.noreply.github.com>
2023-05-24 15:49:42 +0200

    added line for DEV7 object

--- DecompOBC/DecompOBC/DecompOBC.cs
+++ DecompOBC/DecompOBC/DecompOBC.cs
@@ -5,6 +5,7 @@ Console.WriteLine("DecompOBC ver. 1.0");
 Console.WriteLine("Made by CrystalBen");
 Console.WriteLine("How to use?");
 Console.WriteLine("type DecompOBC.exe then enter, type your OBC Script Path including your .obc File (without quotes), then it shows you the Text of the Script.");
+Console.WriteLine("OBC Scripts (.obc) Files are also known as DEV7 object)
 Console.WriteLine("\n");
 Console.WriteLine("Write here the Path to the OBC File");
 
