43926187b69f76a204f7b4f573bf4324c55c56a0
Benjamin Funke <58399929+BJNFNE@users.noreply.github.com>
2023-05-28 21:27:13 +0200

    added some final featured

--- LOADER7VC/LOADER7VC/LOADER7VC.cs
+++ LOADER7VC/LOADER7VC/LOADER7VC.cs
@@ -4,29 +4,42 @@ using System.Reflection;
 string dir = AppDomain.CurrentDomain.BaseDirectory;
 
 
-Console.WriteLine("Welcome to LOADER7VC, This program shows you which version your LOADER7.EXE / Dev7VM.EXE is.");
-Console.WriteLine("\n");
-Console.WriteLine("LOADER7VC was designed for DEV7 Games.");
-Console.WriteLine("\n");
-Console.WriteLine("Copyright: BJNFNE (Benjamin Funke) & DerCodeDev (Samir Ademi)");
-Console.WriteLine("\n");
+// Gets the FileVersion of LOADER7.EXE / Dev7VM.EXE.
+    Console.WriteLine("Please insert here your Path to your LOADER7.EXE / Dev7VM.EXE (Without Quotes)");
+    Console.WriteLine("\b");
+    string EXEInput = Console.ReadLine();
 
-
-{
-    // Gets the FileVersion of LOADER7.EXE / Dev7VM.EXE.
-    Console.WriteLine("Please insert here your Path to your LOADER7.EXE / Dev7VM.EXE");
-    string UserInput = Console.ReadLine();
-
-    FileVersionInfo myFileVersionInfo = FileVersionInfo.GetVersionInfo(fileName: UserInput);
+    FileVersionInfo myFileVersionInfo = FileVersionInfo.GetVersionInfo(fileName: EXEInput);
 
     // Print the Description and the Version.
-    String text = "Description: " + myFileVersionInfo.FileDescription + '\n' +
-    "Version: " + myFileVersionInfo.FileVersion;
-    Console.WriteLine(text);
-    Console.WriteLine("\n");
+    string Description = "Description: " + myFileVersionInfo.FileDescription;
+    string Version = "Version: " + myFileVersionInfo.FileVersion;
+    Console.WriteLine("\b");
+    Console.WriteLine(Description);
+    Console.WriteLine(Version);
+
+     // Print General Infos about LOADER7.EXE / Dev7VM.EXE
+    Console.Write("\n");
+    Console.WriteLine("General Infos");
+    Console.WriteLine("\b");
+    string OriginalFileName = "Original Filename: " + myFileVersionInfo.OriginalFilename;
+    Console.WriteLine(OriginalFileName);
+
+    string ProductName = "Productname: " + myFileVersionInfo.ProductName; 
+    Console.WriteLine(ProductName);
+
+    string InternalName = "Internalname: " + myFileVersionInfo.InternalName;
+    Console.WriteLine(InternalName);
+
+    string Copyright = "Copyright: " + myFileVersionInfo.LegalCopyright;
+    Console.WriteLine(Copyright);
+
+    
+    // executes Thank message
+        Console.WriteLine("\b");
     Console.WriteLine("Thanks for using LOADER7VC!");
-
-
+    
+    
     // This exits LOADER7VC
    
     Console.WriteLine("\nPress any key to exit LOADER7VC");
