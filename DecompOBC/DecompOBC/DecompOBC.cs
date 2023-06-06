74719ad6beead80bede643154fc6de7cc7b86426
Benjamin Funke <58399929+BJNFNE@users.noreply.github.com>
2023-06-07 01:26:39 +0200

    changed name CrystalBen to BJNFNE

--- DecompOBC/DecompOBC/DecompOBC.cs
+++ DecompOBC/DecompOBC/DecompOBC.cs
@@ -2,7 +2,7 @@
 using System.IO;
 using System.Text;
 Console.WriteLine("DecompOBC ver. 1.0");
-Console.WriteLine("Made by CrystalBen");
+Console.WriteLine("Made by BJNFNE");
 Console.WriteLine("How to use?");
 Console.WriteLine("type DecompOBC.exe then enter, type your OBC Script Path including your .obc File (without quotes), then it shows you the Text of the Script.");
 Console.WriteLine("OBC Scripts (.obc) Files are also known as DEV7 object");
