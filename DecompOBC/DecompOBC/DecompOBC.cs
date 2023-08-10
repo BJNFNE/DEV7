734d42cd02328bf59ca668168aea219240d88b8e
Benjamin Funke <58399929+BJNFNE@users.noreply.github.com>
2023-08-10 20:19:37 +0200

    implemented Output File for OBC
    Implemented an obc_output.txt that is storing the content of the OBC Script instead of showing the output in the Console.

--- DecompOBC/DecompOBC/DecompOBC.cs
+++ DecompOBC/DecompOBC/DecompOBC.cs
@@ -1,22 +1,30 @@
-﻿using System;
+using System;
+using System.Diagnostics;
 using System.IO;
 using System.Text;
-Console.WriteLine("DecompOBC ver. 1.0");
-Console.WriteLine("Made by BJNFNE");
-Console.WriteLine("How to use?");
-Console.WriteLine("type DecompOBC.exe then enter, type your OBC Script Path including your .obc File (without quotes), then it shows you the Text of the Script.");
-Console.WriteLine("OBC Scripts (.obc) Files are also known as DEV7 object");
-Console.WriteLine("\n");
-Console.WriteLine("Write here the Path to the OBC File");
+using System.Text.RegularExpressions;
+using static System.Net.Mime.MediaTypeNames;
 
+Console.WriteLine("DecompOBC ver. 1.2");
+Console.WriteLine("Made by BJNFNE");
+Console.WriteLine("\n");
+Console.WriteLine("Write here the Path to the OBC File, after that a obc_output.txt gets created that contains the decompiled code.");
+
+// only allows ".obc"
+string myFilePath = @".obc";
+string ext = Path.GetExtension(myFilePath);
 string OBC_OUTPUT = Console.ReadLine();
 
+// Creates OBC Output File
+FileStream filestream = new FileStream("obc_output.txt", FileMode.Create);
+var streamwriter = new StreamWriter(filestream);
+streamwriter.AutoFlush = true;
+Console.SetOut(streamwriter);
+Console.SetError(streamwriter);
 
 {
     Console.WriteLine("\n");
-            // Output the Text of the OBC Script.
-            Console.WriteLine(File.ReadAllText(@OBC_OUTPUT));
-
-
+    // Extracts the Text of the OBC Script.
+    Console.WriteLine(File.ReadAllText(@OBC_OUTPUT));
 
 }
