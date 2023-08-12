a094ecc9ea66dc0f677fee8737e0b2ee9e215488
Benjamin Funke <58399929+BJNFNE@users.noreply.github.com>
2023-08-12 15:43:48 +0200

    remove unneeded functions

--- DecompOBC/DecompOBC/DecompOBC.cs
+++ DecompOBC/DecompOBC/DecompOBC.cs
@@ -2,8 +2,6 @@ using System;
 using System.Diagnostics;
 using System.IO;
 using System.Text;
-using System.Text.RegularExpressions;
-using static System.Net.Mime.MediaTypeNames;
 
 Console.WriteLine("DecompOBC ver. 1.2");
 Console.WriteLine("Made by BJNFNE");
