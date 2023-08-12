14edb043f075fac05aa8dfdb736b9f32dddcc2fb
Benjamin Funke <58399929+BJNFNE@users.noreply.github.com>
2023-08-12 19:54:59 +0200

    changed message of Entering the Path to the OBC Script

--- DecompOBC/DecompOBC/DecompOBC.cs
+++ DecompOBC/DecompOBC/DecompOBC.cs
@@ -7,7 +7,7 @@ Console.Title = "DecompOBC 1.2";
 
 Console.WriteLine("Made by BJNFNE");
 Console.WriteLine("\b");
-Console.WriteLine("Write here the Path to the OBC File, after that a obc_output.txt gets created that contains the decompiled code.");
+Console.WriteLine("Enter here the Path to the OBC Script, after that obc_output.txt gets created that contains the decompiled code.");
 
 // only allows ".obc"
 string myFilePath = @".obc";
