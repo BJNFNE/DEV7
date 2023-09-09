c106c73ce88a00bd58034b0c988b5bb68a3aba05
Benjamin Funke <58399929+BJNFNE@users.noreply.github.com>
2023-09-09 14:45:28 +0200

    improved DecompOBC's code

--- DecompOBC/DecompOBC.cpp
+++ DecompOBC/DecompOBC.cpp
@@ -5,21 +5,23 @@ int main()
     char ch, inputOBC[20], outputOBC[20];
     FILE *fs, *ft;
     cout<<"Developed by BJNFNE\n";
-    cout<<"WIP - the decompiled Scripts can eventually display non printable Characters\n";
-    cout<<"Enter the Name of the OBC Script: ";
+    cout<<"WIP - the decompiled Scripts can eventually display non printable Characters.\n";
+    cout<<"Enter the Name of the OBC Script that should be decompiled: ";
     cin>>inputOBC;
     fs = fopen(inputOBC, "r");
     if(fs == NULL)
     {
-        cout<<"\nError Occurred!";
+        cout<<"\nOBC Script not found!\n";
+
         return 0;
     }
-    cout<<"\nEnter the Path of the OBC Script: ";
+    cout<<"\nOBC Script was found!\n";
+    cout<<"\nEnter here your output filename including extension (TXT): ";
     cin>>outputOBC;
     ft = fopen(outputOBC, "w");
     if(ft == NULL)
     {
-        cout<<"\nError Occurred!";
+        cout<<"\nError Occurred, by creating the output file!\n";
         return 0;
     }
     ch = fgetc(fs);
