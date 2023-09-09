8c59a78d12a5a7d03f3053ba015c6b5f877c17c3
Benjamin Funke <58399929+BJNFNE@users.noreply.github.com>
2023-09-09 12:12:11 +0200

    adding new Directory of DecompOBC (C++)

---
+++ DecompOBC/DecompOBC.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    char ch, inputOBC[20], outputOBC[20];
+    FILE *fs, *ft;
+    cout<<"Enter the Name of the OBC Script: ";
+    cin>>inputOBC;
+    fs = fopen(inputOBC, "r");
+    if(fs == NULL)
+    {
+        cout<<"\nError Occurred!";
+        return 0;
+    }
+    cout<<"\nEnter the Path of the OBC Script: ";
+    cin>>outputOBC;
+    ft = fopen(outputOBC, "w");
+    if(ft == NULL)
+    {
+        cout<<"\nError Occurred!";
+        return 0;
+    }
+    ch = fgetc(fs);
+    while(ch != EOF)
+    {
+        fputc(ch, ft);
+        ch = fgetc(fs);
+    }
+    cout<<"\nOBC Script is decompiled successfully.";
+    fclose(fs);
+    fclose(ft);
+    cout<<endl;
+    return 0;
+}
