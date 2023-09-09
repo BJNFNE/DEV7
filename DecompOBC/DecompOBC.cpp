8b62a746dab610778f26337294837271d12f012b
Benjamin Funke <58399929+BJNFNE@users.noreply.github.com>
2023-09-09 21:55:11 +0200

    renamed some variables

--- DecompOBC/DecompOBC.cpp
+++ DecompOBC/DecompOBC.cpp
@@ -3,13 +3,13 @@ using namespace std;
 int main()
 {
     char ch, inputOBC[20], outputOBC[20];
-    FILE *fs, *ft;
+    FILE *robc, *wobc;
     cout<<"Developed by BJNFNE\n";
     cout<<"WIP - the decompiled Scripts can eventually display non printable Characters.\n";
     cout<<"Enter the Name of the OBC Script that should be decompiled: ";
     cin>>inputOBC;
-    fs = fopen(inputOBC, "r");
-    if(fs == nullptr)
+    robc = fopen(inputOBC, "r"); // robc stands for ReadOBC &  wobc stands for WriteOBC
+    if(robc == nullptr)
     {
         cout<<"\nOBC Script not found!\n";
 
@@ -18,21 +18,21 @@ int main()
     cout<<"\nOBC Script was found!\n";
     cout<<"\nEnter here your output filename including extension (TXT): ";
     cin>>outputOBC;
-    ft = fopen(outputOBC, "w");
-    if(ft == nullptr)
+    wobc = fopen(outputOBC, "w");
+    if(wobc == nullptr)
     {
         cout<<"\nError Occurred, by creating the output file!\n";
         return 0;
     }
-    ch = fgetc(fs);
+    ch = fgetc(robc);
     while(ch != EOF)
     {
-        fputc(ch, ft);
-        ch = fgetc(fs);
+        fputc(ch, wobc);
+        ch = fgetc(robc);
     }
     cout<<"\nOBC Script is decompiled successfully.";
-    fclose(fs);
-    fclose(ft);
+    fclose(robc);
+    fclose(wobc);
     cout<<endl;
     return 0;
 }
