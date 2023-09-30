3e76f593067def07597f8a1864522e244719653a
Benjamin Funke <58399929+BJNFNE@users.noreply.github.com>
2023-09-30 15:35:53 +0200

    make an better overview with some more space

--- IMGIdentifier/IMGIdentifier.py
+++ IMGIdentifier/IMGIdentifier.py
@@ -8,16 +8,20 @@ def identify_img(file_path, img_check):
         pass
     return False
 
+print("\b")
 img_file = input("Enter the name of the IMG file (with .IMG extension): ")
 
 if img_file.lower().endswith((".img", ".IMG")):
     if identify_img(img_file, "TRUEVISION-XFILE"):
         print("It's a TGA file")
     elif identify_img(img_file, "IMG10"):
-        print("Its an IMG file (version 1.0)")
+        print("\b")
+        print("IMG using version 1.0")
+        print("\b")
     elif identify_img(img_file, "IMG1.0.1"):
-        print("It's an IMG file (version 1.0.1)")
-    
+        print("\b")
+        print("IMG using version 1.0.1")
+        print("\b")
     else:
         print("IMG format not recognized")
         print("Maybe your IMG uses an different format, Then please contact BJNFNE on Discord\n")
