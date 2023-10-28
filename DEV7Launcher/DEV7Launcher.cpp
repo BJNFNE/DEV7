4c1d25a9c24cb1ec4c4fba890344d524f224efc9
Benjamin Funke <58399929+BJNFNE@users.noreply.github.com>
2023-10-28 08:05:22 +0200

    improve the Launcher

--- DEV7Launcher/DEV7Launcher.cpp
+++ DEV7Launcher/DEV7Launcher.cpp
@@ -22,48 +22,43 @@ bool fileExists(const std::string& filename) {
 #endif
 }
 
-void launchDEV7(const std::string& dev7Command, const std::string& loaderCommand) {
+void launchCommand(const std::string& command) {
 #ifdef _WIN32
-    if (fileExists("Dev7VM.exe")) {
-        system(dev7Command.c_str());
-    } else if (fileExists("LOADER7.EXE") || fileExists("loader7.exe")) {
-        system(loaderCommand.c_str());
-    } else {
-        std::cerr << "No suitable DEV7 client found. Dev7VM.exe, LOADER7.EXE, or loader7.exe is missing." << std::endl;
-    }
-    system(SLEEP_COMMAND);
+    system(command.c_str());
 #else
     // On Linux, we always try running the command with Wine
-    std::string wineCommand = "wine " + dev7Command;
-    if (fileExists("Dev7VM.exe")) {
-        system(wineCommand.c_str());
-        system(SLEEP_COMMAND);
-    } else if (fileExists("LOADER7.EXE") || fileExists("loader7.exe")) {
-        std::string wineLoaderCommand = "wine " + loaderCommand;
-        system(wineLoaderCommand.c_str());
-        system(SLEEP_COMMAND);
-    } else {
-        std::cerr << "No suitable DEV7 client found. Dev7VM.exe, loader7.exe is missing." << std::endl;
-    }
+    std::string wineCommand = "wine " + command;
+    system(wineCommand.c_str());
 #endif
+    system(SLEEP_COMMAND);
 }
 
 int main(int argc, char* argv[]) {
     int choice;
 
-    std::string dev7VMCommand = "Dev7VM.exe -break";
-    std::string loaderCommand = "loader7.exe -break";
+    std::string dev7VMDebugCommand = "Dev7VM.exe -break";
+    std::string dev7VMNormalCommand = "Dev7VM.exe";
+    std::string loaderDebugCommand = "loader7.exe -break";
+    std::string loaderNormalCommand = "loader7.exe";
 
     std::cout << "Choose an option:\n";
-    std::cout << "1. Start DEV7 with Debug Menu\n";
-    std::cout << "2. Start DEV7 normal\n";
+    std::cout << "1. Start Dev7VM with Debug Menu\n";
+    std::cout << "2. Start Dev7VM Normal\n";
+    std::cout << "3. Start loader7 with Debug Menu\n";
+    std::cout << "4. Start loader7 Normal\n";
     std::cin >> choice;
     std::cout << "\n";
 
-    if (choice == 1 || choice == 2) {
-        launchDEV7(dev7VMCommand, loaderCommand);
+    if (choice == 1) {
+        launchCommand(dev7VMDebugCommand);
+    } else if (choice == 2) {
+        launchCommand(dev7VMNormalCommand);
+    } else if (choice == 3) {
+        launchCommand(loaderDebugCommand);
+    } else if (choice == 4) {
+        launchCommand(loaderNormalCommand);
     } else {
-        std::cout << "Invalid choice. Please choose 1 or 2." << std::endl;
+        std::cout << "Invalid choice. Please choose 1, 2, 3, or 4." << std::endl;
     }
 
     return 0;
