6327044460e5b9339be1e9018fd3594badc53764
Benjamin Funke <58399929+BJNFNE@users.noreply.github.com>
2023-11-28 14:44:22 +0100

    add an note for handling currently on IMG's (TGA)

--- IMGConverter/IMGConverter.py
+++ IMGConverter/IMGConverter.py
@@ -1,5 +1,7 @@
 from PIL import Image
 import os
+print(f"\b")
+print(f"Currently IMGConverter only can convert IMG's that are based on TGA.\f")
 
 # Prompt the user for the input image file path
 img_input = input("Enter the path of the input image file: ")
@@ -28,3 +30,4 @@ original_image = Image.open(img_input)
 original_image.save(output_image)
 
 print(f"Image converted from {img_input} to {output_image} successfully.")
+
