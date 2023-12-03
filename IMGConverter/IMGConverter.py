9b737d2acaf0c54917ec89bfa7fce903595fec2a
Benjamin Funke <58399929+BJNFNE@users.noreply.github.com>
2023-12-03 10:23:44 +0100

    add error handling for screen resolution

--- IMGConverter/IMGConverter.py
+++ IMGConverter/IMGConverter.py
@@ -15,20 +15,24 @@ def main():
     input_filename, input_extension = os.path.splitext(os.path.basename(input_image_path))
 
     # Choose screen resolution
-    resolution_choice = input("Choose screen resolution (e.g., 1920x1080): ")
-    width, height = map(int, resolution_choice.split('x'))
-    resolution = (width, height)
+    resolution_choice = input("Choose screen resolution: ")
+    
+    if resolution_choice:
+        width, height = map(int, resolution_choice.split('x'))
+        resolution = (width, height)
 
-    # Convert to TGA (temporary format)
-    temp_output_path = f"{input_filename}_temp.tga"
-    convert_to_tga(input_image_path, temp_output_path, resolution)
-    print(f"Image converted to TGA with resolution {resolution}: {temp_output_path}")
+        # Convert to TGA (temporary format)
+        temp_output_path = f"{input_filename}_temp.tga"
+        convert_to_tga(input_image_path, temp_output_path, resolution)
+        print(f"Image converted to TGA with resolution {resolution}: {temp_output_path}")
 
-    # Choose the output filename for IMG
-    img_output_filename = input("Enter the desired output filename for IMG (without extension): ")
-    img_output_path = f"{img_output_filename}.img"
-    os.rename(temp_output_path, img_output_path)
-    print(f"TGA renamed to IMG: {img_output_path}")
+        # Choose the output filename for IMG
+        img_output_filename = input("Enter the desired output filename for IMG (without extension): ")
+        img_output_path = f"{img_output_filename}.img"
+        os.rename(temp_output_path, img_output_path)
+        print(f"TGA renamed to IMG: {img_output_path}")
+    else:
+        print("Please enter an valid resolution.")
 
 if __name__ == "__main__":
     main()
