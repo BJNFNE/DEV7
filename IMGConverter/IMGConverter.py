dab3d8dcd5d37be66886bcc5402b2408ba2e44dd
Benjamin Funke <58399929+BJNFNE@users.noreply.github.com>
2023-12-17 12:07:48 +0100

    add allowing only specfic file formats for input file

--- IMGConverter/IMGConverter.py
+++ IMGConverter/IMGConverter.py
@@ -2,6 +2,12 @@ from PIL import Image
 import os
 import sys
 
+ALLOWED_FORMATS = {".jpg", ".jpeg", ".png", ".bmp"}
+
+def is_valid_image_file(file_path):
+    _, file_extension = os.path.splitext(file_path)
+    return file_extension.lower() in ALLOWED_FORMATS
+
 def convert_to_tga(input_path, output_path, resolution):
     image = Image.open(input_path)
     # Resize the image to the chosen resolution
@@ -14,10 +20,10 @@ def is_resolution_valid(resolution):
 
     width, height = resolution
     assert width <= max_width and height <= max_height, f"The maximum resolution is {max_width}x{max_height}"
-    
+
 def main():
     try:
-        # Input path from user input
+        # Enter Input file from user input
         input_image_path = input("Enter the path to the input image: ")
 
         # Check if the input file exists
@@ -25,12 +31,17 @@ def main():
             print(f"Error: Input file not found")
             sys.exit(1)
 
+        # Check if the input file has an allowed extension
+        if not is_valid_image_file(input_image_path):
+            print(f"Error: Unsupported file format.")
+            sys.exit(1)
+
         # Extract the input filename and extension
         input_filename, input_extension = os.path.splitext(os.path.basename(input_image_path))
 
         # Choose screen resolution
         resolution_choice = input("Choose screen resolution: ")
-        
+
         if resolution_choice:
             width, height = map(int, resolution_choice.split('x'))
             resolution = (width, height)
