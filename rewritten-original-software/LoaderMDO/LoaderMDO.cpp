b6d94be48d8e6a78b1fc73d6b108c0219834d6e4
Benjamin Funke <58399929+BJNFNE@users.noreply.github.com>
2024-03-11 16:57:39 +0100

    add notes about the batch files to compile LoaderMDO

--- rewritten-original-software/LoaderMDO/LoaderMDO.cpp
+++ rewritten-original-software/LoaderMDO/LoaderMDO.cpp
@@ -3,14 +3,15 @@ LoaderMDO was used as Adibou3.EXE in Adibou 3 and as ADI5.EXE in ADI 5.
 It was developed to be linked to Loader7.exe to run it.
 ***********************************************************************
 This is just an attempt to recreate that EXE in C++ as it was developed.
-It was originally developed for ADI 5 version 5.00 in the Year 2000
+It was originally developed for ADI 5 version 5.00 in the Year 2000.
 Requirements:
 -  Compile under MSVC 6.0
 Compiling:
-- Compile in Release mode to get near the filesize of the original exe.
+- Compile in Release mode to keep it original in Release mode as it was back then. (directly from Visual Studio 6.0)
+- build_release.bat: if you compile it over this batch file it whil be smaller then original in size.
+- build_debug.bat: if you compile over that batch file you have the exact same filesize with debug symbols included.
 TODO:
-- Try to match the original filesize (24kb)
-- Try to strip out the Console window completely
+- Try to avoid to create an Console window if the executable is being running.
 */
 
 #include <windows.h>
@@ -30,4 +31,3 @@ TODO:
 
     return 0;
 }
-
