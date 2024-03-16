777be171adec8c9335c2307fa8e2c0c491061d4d
Benjamin Funke <58399929+BJNFNE@users.noreply.github.com>
2024-03-16 11:11:54 +0100

    remove deps for MSVCRT in release mode of LoaderMDO

--- rewritten-original-software/LoaderMDO/build_release.bat
+++ rewritten-original-software/LoaderMDO/build_release.bat
@@ -1,6 +1,6 @@
 @echo off
 title Release Mode Compile Script for LoaderMDO
-echo Welcome to the LoaderMDO compilation script!
+echo Welcome to the LoaderMDO compilation script (Release mode)!
 
 REM Prompt the user to compile in release mode
 set /p compile_release=Compile LoaderMDO in release mode? (yes/no): 
@@ -9,7 +9,7 @@ REM Check the user input
 if /i "%compile_release%"=="yes" (
     echo Compiling LoaderMDO in release mode...
     REM Compile the source file
-    cl.exe /O2 /EHsc /MD /c LoaderMDO.cpp
+    cl.exe /O2 /EHsc /c /MT LoaderMDO.cpp
     REM Link the object file
     link.exe /OUT:LoaderMDO.exe LoaderMDO.obj user32.lib
 ) else if /i "%compile_release%"=="no" (
