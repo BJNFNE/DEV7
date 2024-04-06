09bc6ab0e488745c6c741cc842edf9d58ea91861
Benjamin Funke <bjnfne@web.de>
2024-04-06 11:22:07 +0200

    add convert_bin to common tools

---
+++ common-tools/convert_bin/convert_bin.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+// inputISO for registering which ISO should be converted. 
+std::string inputISO;
+std::cout << "Navigate to the Path of your ISO!" << std::endl;
+std::cin >> inputISO;
+
+std::string outputBIN;
+std::cout << "Give me the Output directory for the BIN file" << std::endl;
+std::cin >> outputBIN;
+
+// Setup here the Program for converting iso to bin
+
+std::string converter = "dd if=" + inputISO + " of=" + outputBIN + " bs=2048 conv=sync,notrunc";
+
+// Execute final Converter command.
+system(converter.c_str());
+
+
+return 0;
+
+}
