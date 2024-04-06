fe4815fdcec7c3385c31532141834413e80a7996
Benjamin Funke <bjnfne@web.de>
2024-04-06 14:31:37 +0200

    add create_zip to repository

---
+++ common-tools/create_zip/create_zip.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+// inputFolder for selecting which Folder should be used for the creation of the ZIP archive. 
+std::string inputFolder;
+std::cout << "Navigate to the Path of the input Folder!" << std::endl;
+std::cin >> inputFolder;
+
+std::string outputZIP;
+std::cout << "Give me the Output directory for the ZIP archive" << std::endl;
+std::cin >> outputZIP;
+
+// Setup here the Program for creating ZIP archive from the Folder.
+
+std::string zipCommand = "zip -r " + outputZIP + " " + inputFolder;
+
+// Execute final Converter command.
+system(zipCommand.c_str());
+
+
+return 0;
+
+}
