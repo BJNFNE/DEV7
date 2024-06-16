8372664944235aa87d5fc117ce95f62cb36fe636
Benjamin Funke <58399929+BJNFNE@users.noreply.github.com>
2024-06-16 16:29:56 +0200

    add ExeStr.cpp to source

---
+++ source/ExeStr.cpp
@@ -0,0 +1,28 @@
+
+void ExeStrNbPos() {
+
+  // Implement the Function ExeStrNbPos()
+  
+  return 0;
+}
+
+void ExeStrPos() {
+
+  // Implement the Function ExeStrPos()
+  
+  return 0;
+}
+
+void ExeStrNbElts() {
+
+  // Implement the Function ExeStrNbElts()
+  
+  return 0;
+}
+
+void ExeStrElt() {
+
+  // Implement the Function ExeStrElt()
+  
+  return 0;
+}
