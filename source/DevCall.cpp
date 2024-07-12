d550e41fdccdb46655fb3740a5150dafb0c7eda5
Benjamin Funke <58399929+BJNFNE@users.noreply.github.com>
2024-07-12 11:18:44 +0200

    add DevCall.cpp to source

---
+++ source/DevCall.cpp
@@ -0,0 +1,39 @@
+void CallDiff() {
+
+// Add implementation for CallDiff()
+  
+return 0;
+  
+}
+
+void CallImmediat() {
+
+// Add implementation for CallImmediat()
+  
+return 0;
+  
+}
+
+void DoCallDiff() {
+
+// Add implementation for DoCallDiff()
+  
+return 0;
+  
+}
+
+void 1CALLSTRUCT() {
+
+// Add implementation for 1CALLSTRUCT()
+  
+return 0;
+  
+}
+
+void objFlushMessage() {
+
+// Add implementation for objFlushMessage()
+  
+return 0;
+  
+}
