4f46cf8b70d214aeda4a4e73e7907c78c9e2dbbf
Benjamin Funke <58399929+BJNFNE@users.noreply.github.com>
2024-07-12 11:44:27 +0200

    add vesa32.cpp to soruce

---
+++ source/Libs Graph/vesa32.cpp
@@ -0,0 +1,11 @@
+#define _InitHalfFill
+#define _Move80
+#define _MoveC0
+#define _Move40
+#define _MoveDOUBLEY
+#define _GetSector
+#define _GRfillZoneVESA
+#define _GRpictMoveVESA
+#define _GRplotVESA
+#define _GRinvertRectVESA
+#define _GRinvertColorRect
