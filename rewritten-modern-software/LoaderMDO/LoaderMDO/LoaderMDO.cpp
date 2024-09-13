b0f6f1b7f5d1c25ceea8f873b08a23d8627ebb9d
Benjamin Funke <58399929+BJNFNE@users.noreply.github.com>
2024-09-13 18:22:26 +0200

    use nullptr instead of NULL in modern version of LoaderMDO

--- rewritten-modern-software/LoaderMDO/LoaderMDO/LoaderMDO.cpp
+++ rewritten-modern-software/LoaderMDO/LoaderMDO/LoaderMDO.cpp
@@ -35,7 +35,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     }
 
     // Launch Loader7.exe using _spawnl
-    _spawnl(_P_NOWAIT, "Loader7.exe", "Loader7.exe", "Loader7.exe", NULL);
+    _spawnl(_P_NOWAIT, "Loader7.exe", "Loader7.exe", "Loader7.exe", nullptr);
 
     // Close the mutex handle after the process is created
     CloseHandle(MutexA);
