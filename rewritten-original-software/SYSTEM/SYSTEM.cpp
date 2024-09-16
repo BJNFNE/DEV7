e1950debcc39c0fe546597d1fa0b1408532f9023
Benjamin Funke <58399929+BJNFNE@users.noreply.github.com>
2024-09-16 17:43:20 +0200

    add SYSTEM to repository

---
+++ rewritten-original-software/SYSTEM/SYSTEM.cpp
@@ -0,0 +1,22 @@
+#include "stdafx.h"  // Precompiled header for Visual Studio 6.0
+#include <windows.h>
+#include <string.h>
+#include <stdlib.h>
+
+// Define the actual value of byte_405490
+char comparisonString[] = "0";
+
+int __stdcall WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
+{
+    int result;
+
+    // Compare the command-line argument with the value stored in byte_405490
+    result = strcmp(lpCmdLine, comparisonString);
+
+    // If the command line argument doesn't match, return the integer version of lpCmdLine
+    if (result != 0)
+        return atoi(lpCmdLine);
+
+    // If the strings match, return the result (which would be 0 from strcmp)
+    return result;
+}
