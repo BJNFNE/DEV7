48312bf5dd22e8b8f7f97ab05b0e68dd35d2c1f7
Benjamin Funke <bjnfne@web.de>
2024-09-16 19:33:56 +0200

    add description for SYSTEM.EXE in SYSTEM.cpp

--- rewritten-original-software/SYSTEM/SYSTEM.cpp
+++ rewritten-original-software/SYSTEM/SYSTEM.cpp
@@ -1,3 +1,17 @@
+/*
+SYSTEM was developed to check if the Disc is valid or not.
+This version is from 2000 that which one doesn't used SecuROM yet.
+SecuROM was started to being used in 2001/2002 in SYSTEM.EXE.
+
+Requirements:
+-  Compile under MSVC 6.0 build 8168 (Any newer build of MSVC6.0 work aswell but Coktel/MDO used back then used exactly this version of the Compiler.)
+
+Compiling:
+- build_release.bat: Run the batch script and confirm with yes to compile.
+
+Based on system.exe MD5: d956cf751400520075cd5910673f5a1f Date: Jun 10 2000
+*/
+
 #include "stdafx.h"
 #include <windows.h>
 #include <string.h>
