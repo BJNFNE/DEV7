71d850c005afabb20b2116f4441b4cd0399f9c52
Benjamin Funke <58399929+BJNFNE@users.noreply.github.com>
2024-09-17 17:33:49 +0200

    add modern version of SYSTEM

---
+++ rewritten-modern-software/SYSTEM/SYSTEM/SYSTEM.cpp
@@ -0,0 +1,36 @@
+/*
+SYSTEM was developed to check if the Disc is valid or not.
+This version is from 2000 that which one doesn't used SecuROM yet.
+SecuROM was started to being used in 2001/2002 in SYSTEM.EXE.
+
+Requirements:
+-  Compile under MSVC 6.0 build 8168 (Any newer build of MSVC6.0 work aswell but Coktel/MDO used back then used exactly this version of the Compiler.)
+
+Compiling:
+- build_release.bat: Run the batch script and confirm with yes to compile.
+
+Based on system.exe MD5: d956cf751400520075cd5910673f5a1f Date: Jun 10 2000
+*/
+
+#include "pch.h"
+#include <windows.h>
+#include <string.h>
+#include <stdlib.h>
+
+// Define comparisonString to be 0
+char comparisonString[] = "0";
+
+int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
+{
+    int result;
+
+    // Compare the command-line argument with the value stored in comparisonString
+    result = strcmp(lpCmdLine, comparisonString);
+
+    // If the command line argument doesn't match, return the integer version of lpCmdLine
+    if (result != 0)
+        return atoi(lpCmdLine);
+
+    // If the strings match, return the result (which would be 0 from strcmp)
+    return result;
+}
