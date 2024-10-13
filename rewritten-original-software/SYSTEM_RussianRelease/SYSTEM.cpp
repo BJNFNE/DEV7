7fd4db6512e7209412e95118101ed4df910328aa
Benjamin Funke <bjnfne@web.de>
2024-10-13 10:43:54 +0200

    adjust russian release of SYSTEM just for Антошка3

--- rewritten-original-software/SYSTEM_RussianRelease/SYSTEM.cpp
+++ rewritten-original-software/SYSTEM_RussianRelease/SYSTEM.cpp
@@ -1,5 +1,6 @@
 /*
 SYSTEM was developed to check if the Disc is valid or not.
+It was mainly re-developed for Антошка3 Applications.
 This version comes from the russian release and is slightly different from the original SYSTEM executable.
 
 Requirements:
@@ -14,7 +15,6 @@ TODO:
 
 Supported Games:
 - Антошка3 (Fully supported)
-- Adiboud'chou (Fully supported)
 
 Supported Applications:
 - Антошка3
@@ -25,12 +25,8 @@ Supported Applications:
 -- Ñ÷åòíàÿ äîëèíà (Ñ÷åò 6-7 ëåò)
 -- Çàãàäî÷íàÿ ìóçûêàëüíàÿ ìàøèíà (Ìóçûêà, 4-7 ëåò)
 
-- Adiboud'chou
--- Russian:
--- Â ãîñòÿõ ó Òèìîøêè
-
 WORKAROUND:
-- Why some versions of Adiboud'chou and Антошка3 don't accept the executable?
+- Why some versions of Антошка3 don't accept the executable?
 - For some Games you can leave the Environment CD/ISO mounted and mount the Application CD/ISO as an seperate drive.
 - Some Games although needs to be mounted in the same Drive as the Environment so there you have to change the CD/ISO.
 
@@ -45,7 +41,7 @@ Based on system.exe MD5: 2515cf4364dccf43f8002225dfa0d6fa Date: Nov 05 2005 (36K
 
 #include "stdafx.h"
 #include <windows.h>
-#include <stdio.h>
+#include <stdio.h> // Used for MSVCRT
 #include <stdlib.h>
 
 int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
