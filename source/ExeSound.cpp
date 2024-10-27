1694f5f65a329e12f2b4392e55dac09e34e52a05
Benjamin Funke <bjnfne@web.de>
2024-10-27 16:14:36 +0100

    implement SetIsPaused() in ExeSound.cpp

--- source/ExeSound.cpp
+++ source/ExeSound.cpp
@@ -3,10 +3,17 @@ namespace Dev7Sound {
 class SoundManager {
 public:
     void SoundStopNotify(int param_1);
+    void SetIsPaused(int param_1);
 };
 
 void SoundManager::SoundStopNotify(int param_1) {
     *(reinterpret_cast<unsigned int*>(param_1 + 4)) = 2;
+    return;
+}
+
+void SoundManager::SetIsPaused(int param_1) {
+    *(reinterpret_cast<unsigned int*>(param_1 + 4)) = 1;
+    *(reinterpret_cast<unsigned int*>(param_1 + 8)) = param_1;
     return;
 }
 
