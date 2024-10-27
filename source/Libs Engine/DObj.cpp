68fba41ab8ca8e6fe2a6fad239859fd52e2eaa44
Benjamin Funke <bjnfne@web.de>
2024-10-27 17:35:44 +0100

    implement GetFuncByIndex() in DObj.cpp

--- source/Libs Engine/DObj.cpp
+++ source/Libs Engine/DObj.cpp
@@ -3,11 +3,15 @@ namespace DObj {
 class DObj {
 public:
     int GetCurrentTabVar(int param_1);
+    int GetFuncByIndex(int param_1);
 };
 
 int DObj::GetCurrentTabVar(int param_1) {
 return *(int *)(param_1 + 8);
 }
 
+int DObj::GetFuncByIndex(int param_1) {
+return *(int *)(param_1 + 0xc);
+}
 
 } // namespace DObj
