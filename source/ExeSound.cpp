7c70e6905797877e1b0a79628b940ce0948491d5
Benjamin Funke <bjnfne@web.de>
2024-10-27 16:17:12 +0100

    implement IsStopped() in ExeSound.cpp

--- source/ExeSound.cpp
+++ source/ExeSound.cpp
@@ -3,6 +3,7 @@ namespace Dev7Sound {
 class SoundManager {
 public:
     void SoundStopNotify(int param_1);
+    bool IsStopped(int param_1);
     void SetIsPaused(int param_1);
 };
 
@@ -15,6 +16,11 @@ void SoundManager::SetIsPaused(int param_1) {
     *(reinterpret_cast<unsigned int*>(param_1 + 4)) = 1;
     *(reinterpret_cast<unsigned int*>(param_1 + 8)) = param_1;
     return;
+}
+
+bool IsStopped(int param_1) {
+
+return *(int *)(param_1 + 4) ==1;
 }
 
 } // namespace Dev7Sound
