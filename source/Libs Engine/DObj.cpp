469754c6776632c9962f2d0d090bd4ca23ad1139
Benjamin Funke <bjnfne@web.de>
2024-10-27 17:49:29 +0100

    implement AddHandler to DObj.cpp

--- source/Libs Engine/DObj.cpp
+++ source/Libs Engine/DObj.cpp
@@ -2,10 +2,16 @@ namespace DObj {
 
 class DObj {
 public:
+    int AddHandler(int param_1);
     int GetCurrentTabVar(int param_1);
     int GetFuncByIndex(int param_1);
     int GetStackRef(int param_1);
 };
+
+int AddHandler(int param_1) {
+*(short *)(param_1 + 0x3c) = *(short *)(param_1 + 0x3c) + -1;
+return;
+}
 
 int DObj::GetCurrentTabVar(int param_1) {
 return *(int *)(param_1 + 8);
