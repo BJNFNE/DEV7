e118a370dafddc07862cc72a051c9a8bdad44838
Benjamin Funke <bjnfne@web.de>
2024-10-27 17:38:24 +0100

    implement GetStackRef in DObj.cpp

--- source/Libs Engine/DObj.cpp
+++ source/Libs Engine/DObj.cpp
@@ -4,6 +4,7 @@ class DObj {
 public:
     int GetCurrentTabVar(int param_1);
     int GetFuncByIndex(int param_1);
+    int GetStackRef(int param_1);
 };
 
 int DObj::GetCurrentTabVar(int param_1) {
@@ -12,6 +13,12 @@ return *(int *)(param_1 + 8);
 
 int DObj::GetFuncByIndex(int param_1) {
 return *(int *)(param_1 + 0xc);
+}
+
+int DObj::GetStackRef(int param_1) {
+
+*(short *)(param_1 + 0x3c) = *(short *)(param_1 + 0x3c) + 1;
+return;
 }
 
 } // namespace DObj
