dd86423093f7658c9df5e4bc30ea8d3260b9c10c
Benjamin Funke <bjnfne@web.de>
2024-10-29 21:56:18 +0100

    implement SetPosition in ExeStream.cpp

--- source/ExeStream.cpp
+++ source/ExeStream.cpp
@@ -3,10 +3,15 @@ namespace ExeStream {
 class DStream {
 public:
     int GetPosition(int param_1);
+    int SetPosition(int param_1);
 };
 
 int DStream::GetPosition(int param_1) {
     return *(int*)(reinterpret_cast<int>(this) + param_1 + 8);
+}
+
+int DStream::SetPosition(int param_1) {
+return *(int *)(param_1 + 0x10);
 }
 
 }
