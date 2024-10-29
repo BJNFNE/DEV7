bdaff8f9aa36adcb578f59d9e21b87d3994414aa
Benjamin Funke <bjnfne@web.de>
2024-10-29 21:57:34 +0100

    implement AddPosition in ExeStream.cpp

--- source/ExeStream.cpp
+++ source/ExeStream.cpp
@@ -2,9 +2,14 @@ namespace ExeStream {
 
 class DStream {
 public:
+    int AddPosition(int param_1);
     int GetPosition(int param_1);
     int SetPosition(int param_1);
 };
+
+int DStream::AddPosition(int param_1) {
+return *(int *)(param_1 + 0xc);
+}
 
 int DStream::GetPosition(int param_1) {
     return *(int*)(reinterpret_cast<int>(this) + param_1 + 8);
