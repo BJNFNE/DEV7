4236d0c24112e698aef8235106d8b5ff69f69971
Benjamin Funke <bjnfne@web.de>
2024-11-01 08:02:15 +0100

    fix errors in ChannelsPC.cpp

--- source/Libs Tracker/ChannelsPC.cpp
+++ source/Libs Tracker/ChannelsPC.cpp
@@ -3,10 +3,10 @@ namespace ChannelsPC {
 class trkWinMasterChannel {
 public:
     int GetLastReadBlockPosition(int param_1);
-
 };
 
 int trkWinMasterChannel::GetLastReadBlockPosition(int param_1) {
-return *(int *)(param_1 + 0x6);
+    return *(int *)(param_1 + 0x6);
 }
 
+} // namespace ChannelsPC
