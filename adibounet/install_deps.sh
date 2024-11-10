63273bd25c4a01ef1c7f5b69b458bbafea7e3328
Benjamin Funke <bjnfne@web.de>
2024-11-10 09:17:28 +0100

    add install_deps script for adibounet

---
+++ adibounet/install_deps.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Run this script as root user!
+
+# Update package list without sudo
+sudo apt update
+
+# Install sudo first (if not already installed)
+if ! command -v sudo &> /dev/null; then
+  echo "Installing sudo..."
+  apt install sudo -y
+fi
+
+# Install Python 3
+echo "Installing Python 3..."
+sudo apt install python3 -y
+
+# Install Git
+echo "Installing Git..."
+sudo apt install git -y
+
+# Install Screen
+echo "Installing Screen..."
+sudo apt install screen -y
+
+# Print completion message
+echo "Installation of AdibouNet deps is done!"
