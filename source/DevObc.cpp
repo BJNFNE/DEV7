92214ac4e04d145f5e39a3ea994d9c5b0ab66b4c
Benjamin Funke <58399929+BJNFNE@users.noreply.github.com>
2024-12-10 09:52:55 +0100

    add DevObc.cpp to source directory

---
+++ source/DevObc.cpp
@@ -0,0 +1,31 @@
+void DevobcLoad() {
+    // TODO
+}
+
+void DevobcUnLoad() {
+    // TODO
+}
+
+void DevobcIsKindOf() {
+    // TODO
+}
+
+void DevobcIsSonOf() {
+    // TODO
+}
+
+void DevobcIsFunc() {
+    // TODO
+}
+
+void DevobcIsVar() {
+    // TODO
+}
+
+void DevobcGetType() {
+    // TODO
+}
+
+void DevobcIsLoaded () {
+    // TODO
+}
