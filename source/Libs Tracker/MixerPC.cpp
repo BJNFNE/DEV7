9a6fec16311d1333d68360ef0066eb8a1584ca48
Benjamin Funke <bjnfne@web.de>
2025-01-30 18:00:15 +0100

    minor improvements for MixerPC.cpp

--- source/Libs Tracker/MixerPC.cpp
+++ source/Libs Tracker/MixerPC.cpp
@@ -1,20 +1,30 @@
 namespace trkWinMixer {
 
+namespace trkChannel {
+
+class trkChannel {
+public: 
+    int IsStereo(int param_1);
+};
+
+} // namespace trkChannel
+
+
 class trkWinMixer {
 public:
     int GetStereo(int param_1);
-    int GetNbSamplesPerBlock(void *this,uint param_1);
+    int GetNbSamplesPerBlock(uint param_1);
 };
 
 int trkWinMixer::GetStereo(int param_1) {
     return *(int *)(param_1 + 0x14);
 }
 
-int trkWinMixer::GetNbSamplesPerBlock(void *this,uint param_1) {
-    // WIP implementation
-      if (*this <= param_1) {
-    return 0;
-  }
+int trkWinMixer::GetNbSamplesPerBlock(uint param_1) {
+    if (*this <= param_1) {
+        return 0;
+    }
+    return 1;
 }
 
-} // namespace ChannelsPC
+} // namespace trkWinMixer
