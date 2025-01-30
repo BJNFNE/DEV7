b760d1c8f8c6dd2dea44dbd222596ac52be6cf15
Benjamin Funke <bjnfne@web.de>
2025-01-30 17:54:00 +0100

    WIP Implementation of trkWinMixer::GetNbSamplesPerBlock

--- source/Libs Tracker/MixerPC.cpp
+++ source/Libs Tracker/MixerPC.cpp
@@ -3,10 +3,18 @@ namespace trkWinMixer {
 class trkWinMixer {
 public:
     int GetStereo(int param_1);
+    int GetNbSamplesPerBlock(void *this,uint param_1);
 };
 
 int trkWinMixer::GetStereo(int param_1) {
     return *(int *)(param_1 + 0x14);
+}
+
+int trkWinMixer::GetNbSamplesPerBlock(void *this,uint param_1) {
+    // WIP implementation
+      if (*this <= param_1) {
+    return 0;
+  }
 }
 
 } // namespace ChannelsPC
