3943d4b8775e3f3e8560819ff5a24f2fbd67c507
Benjamin Funke <58399929+BJNFNE@users.noreply.github.com>
2025-02-25 11:50:27 +0100

    add DevEditor.cpp

---
+++ source/DevEditor.cpp
@@ -0,0 +1,12 @@
+namespace DevEditor {
+
+}
+class DevEditor {
+    public:
+        void GetDrawCursor(int param_1);
+    };
+
+    void DevEditor::GetDrawCursor(int param_1) {
+    
+        return;
+    }
