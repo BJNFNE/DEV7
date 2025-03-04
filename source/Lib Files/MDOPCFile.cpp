e6acde73ffb6f4b5f14181c594bc43ba22fd3c22
Benjamin Funke <bjnfne@web.de>
2025-03-04 09:28:47 +0100

    add more functions to MDOPCFile.cpp

--- source/Lib Files/MDOPCFile.cpp
+++ source/Lib Files/MDOPCFile.cpp
@@ -7,3 +7,15 @@ void _GXInitFileSystem() {
 
     return;
 }
+
+void _GXCloseFileSystem(void) {
+
+    return;
+}
+
+void __cdecl _GXFileRename(HFILE param_1,LONG param_2,short param_3)
+
+{
+  _llseek(param_1,param_2,(int)param_3);
+  return;
+}
