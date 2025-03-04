725817b1c7e7a0585f77070cf057a52374882de7
Benjamin Funke <bjnfne@web.de>
2025-03-04 10:03:04 +0100

    fix brackets for _GXFileRename

--- source/Lib Files/MDOPCFile.cpp
+++ source/Lib Files/MDOPCFile.cpp
@@ -13,9 +13,8 @@ void _GXCloseFileSystem(void) {
     return;
 }
 
-void __cdecl _GXFileRename(HFILE param_1,LONG param_2,short param_3)
+void __cdecl _GXFileRename(HFILE param_1,LONG param_2,short param_3) {
 
-{
   _llseek(param_1,param_2,(int)param_3);
   return;
 }
