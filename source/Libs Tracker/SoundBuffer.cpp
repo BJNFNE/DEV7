e2b96d1ab85e258f13afc75b8fdc411ef468fa35
Benjamin Funke <bjnfne@web.de>
2025-03-04 10:18:39 +0100

    implement trkSoundBuffer::GetFrequency

--- source/Libs Tracker/SoundBuffer.cpp
+++ source/Libs Tracker/SoundBuffer.cpp
@@ -3,9 +3,15 @@ namespace SoundBuffer {
     class trkSoundBuffer {
     public:
         int GetSizeInBytes(int param_1);
+        int GetFrequency(int param_1);
     };
 
     int trkSoundBuffer::GetSizeInBytes(int param_1) {
         return *(int *)(param_1 + 0x10);
     }
+
+    int trkSoundBuffer::GetFrequency(int param_1) {
+        return *(int *)(param_1 + 4);
+    }
+
 }
