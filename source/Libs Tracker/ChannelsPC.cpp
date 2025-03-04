c5b649d960ead3f2b9c04d0c9896cff244fb2b86
Benjamin Funke <bjnfne@web.de>
2025-03-04 09:58:16 +0100

    implement trkChannel::GetSoundBuffer

--- source/Libs Tracker/ChannelsPC.cpp
+++ source/Libs Tracker/ChannelsPC.cpp
@@ -3,10 +3,15 @@ namespace ChannelsPC {
 class trkWinMasterChannel {
 public:
     int GetLastReadBlockPosition(int param_1);
+    int GetSoundBuffer(int param_1);
 };
 
 int trkWinMasterChannel::GetLastReadBlockPosition(int param_1) {
     return *(int *)(param_1 + 0x6c);
 }
+
+int trkWinMasterChannel::GetSoundBuffer(int param_1) {
+    return *(int *)(param_1 + 0x30);
+} 
 
 } // namespace ChannelsPC
