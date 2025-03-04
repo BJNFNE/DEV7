a47d5ce006ee780e2879cc7a78b7998ce9c2042f
Benjamin Funke <bjnfne@web.de>
2025-03-04 10:03:39 +0100

    give trkChannel own namespace to ChannelPC.cpp

--- source/Libs Tracker/ChannelsPC.cpp
+++ source/Libs Tracker/ChannelsPC.cpp
@@ -4,14 +4,26 @@ class trkWinMasterChannel {
 public:
     int GetLastReadBlockPosition(int param_1);
     int GetSoundBuffer(int param_1);
+    int GetDataToWrite(int param_1);
+};
+
+class trkChannel {
+    public:
+        int GetSoundBuffer(int param_1);
+        int GetDataToWrite(int param_1);
 };
 
 int trkWinMasterChannel::GetLastReadBlockPosition(int param_1) {
     return *(int *)(param_1 + 0x6c);
 }
 
-int trkWinMasterChannel::GetSoundBuffer(int param_1) {
+int trkChannel::GetSoundBuffer(int param_1) {
     return *(int *)(param_1 + 0x30);
-} 
+}
+
+int trkChannel::GetDataToWrite(int param_1) {
+    return *(int *)(param_1 + 0x2c);
+}
 
 } // namespace ChannelsPC
+  // namespace trkChannel
