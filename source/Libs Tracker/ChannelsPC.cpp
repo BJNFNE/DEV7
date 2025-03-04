2843852f63252be08535c09d7a49e61713bbe4f4
Benjamin Funke <bjnfne@web.de>
2025-03-04 10:05:38 +0100

    implement trkChannel::isStereo

--- source/Libs Tracker/ChannelsPC.cpp
+++ source/Libs Tracker/ChannelsPC.cpp
@@ -11,6 +11,7 @@ class trkChannel {
     public:
         int GetSoundBuffer(int param_1);
         int GetDataToWrite(int param_1);
+        int isStereo(int param_1);
 };
 
 int trkWinMasterChannel::GetLastReadBlockPosition(int param_1) {
@@ -23,6 +24,10 @@ int trkChannel::GetSoundBuffer(int param_1) {
 
 int trkChannel::GetDataToWrite(int param_1) {
     return *(int *)(param_1 + 0x2c);
+}
+
+int trkChannel::isStereo(int param_1) {
+    return *(int *)(param_1 + 0x24);
 }
 
 } // namespace ChannelsPC
