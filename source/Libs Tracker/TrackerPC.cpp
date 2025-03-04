dabb910f5d8362ae2d89d2d9d0a74ac8d11835dd
Benjamin Funke <bjnfne@web.de>
2025-03-04 10:56:23 +0100

    implement trkTrackImpl::DecReadPosition

--- source/Libs Tracker/TrackerPC.cpp
+++ source/Libs Tracker/TrackerPC.cpp
@@ -4,3 +4,12 @@ int trkTrack_Impl::GetCallbacks(int param_1) {
     return *(int **)(param_1 + 0x30);
 }
 
+int trkTrack_Impl::DecReadPosition(int param_1) {
+    {
+        *(int *)((int)this + 0x1c) = ((*(int *)((int)this + 0x18) + *(int *)((int)this + 0xc)) - param_1) + param_2;
+        return;
+      }
+}
+
+      
+
--- source/Libs Tracker/TrackerPC.h
+++ source/Libs Tracker/TrackerPC.h
@@ -6,8 +6,9 @@ namespace TrackerPC {
     class trkTrack_Impl {
     public:
     int GetCallbacks(int param_1);
+    int DecReadPosition(int param_1);
 
-    };
+};
 
 } // namespace TrackerPC
 
